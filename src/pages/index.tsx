954e49bf0cbf613db74a2821383ac7f107117bbc
Tristan <heytristaann@gmail.com>
2023-12-21 15:47:51 -0400

    tmp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,9 @@ export default function Home() {
         <h1 className="text-outline-emerald text-center text-[10rem] font-black text-transparent md:text-[15rem] lg:text-[20rem] xl:text-[25rem] 2xl:text-[27rem]">
           SOCIS
         </h1>
+        <p className="bg-gradient-to-r from-gray-200 to-gray-300 bg-clip-text text-center text-lg font-thin text-transparent">
+          Inspiring the next generation of tech innovators.
+        </p>
         <div className="fixed -bottom-4 h-4 w-screen rotate-180 animate-pulse rounded-full opacity-20 shadow-xl shadow-emerald-500"></div>
       </MainWrapper>
     </>
