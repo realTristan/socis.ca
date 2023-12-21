9833f45d1516f04352340bece946ce0c474a80b6
Tristan <heytristaann@gmail.com>
2023-12-21 10:37:37 -0400

    c

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
       <MainWrapper className="stripes-dark-45">
         <div className="flex w-full flex-col items-center justify-center p-10 px-10">
-          <h1 className="bg-gradient-to-br from-emerald-200 to-emerald-400 bg-clip-text text-center text-[20rem] font-black text-transparent">
+          <h1 className="bg-gradient-to-br from-emerald-600 to-emerald-400 bg-clip-text text-center text-[20rem] font-black text-transparent">
             SOCIS
           </h1>
           <p className="bg-gradient-to-br from-white to-gray-300 bg-clip-text text-center text-base font-light text-transparent lg:text-lg">
