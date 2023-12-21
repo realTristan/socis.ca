2acfdf8f622d8863b3024f472cb8fa3b29f2bde4
Tristan <heytristaann@gmail.com>
2023-12-21 15:40:52 -0400

    reduce bottom glow height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <h1 className="text-outline-emerald text-center text-[10rem] font-black text-transparent md:text-[15rem] lg:text-[20rem] xl:text-[25rem] 2xl:text-[27rem]">
           SOCIS
         </h1>
-        <div className="fixed -bottom-6 h-6 w-screen rotate-180 animate-pulse rounded-full opacity-20 shadow-xl shadow-emerald-500"></div>
+        <div className="fixed -bottom-4 h-4 w-screen rotate-180 animate-pulse rounded-full opacity-20 shadow-xl shadow-emerald-500"></div>
       </MainWrapper>
     </>
   );
