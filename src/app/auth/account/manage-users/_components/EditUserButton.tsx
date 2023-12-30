41671c840df07d72ddd297bdab0791f9cb01a3c4
tristan <heytristaann@gmail.com>
2023-12-30 17:47:41 -0500

    chore: cleanup

--- src/app/auth/account/manage-users/_components/EditUserButton.tsx
+++ src/app/auth/account/manage-users/_components/EditUserButton.tsx
@@ -18,11 +18,7 @@ export default function EditUserButton(
     <button
       className="rounded-lg border border-emerald-500 px-4 py-2 text-base font-thin tracking-wider text-white duration-300 ease-in-out hover:bg-emerald-950/50 disabled:opacity-50"
       disabled={userUpdateStatus === Status.LOADING}
-      onClick={() => {
-        isEditingCurrentUser
-          ? editingUser.set(undefined)
-          : editingUser.set(user);
-      }}
+      onClick={() => editingUser.set(isEditingCurrentUser ? undefined : user)}
     >
       {isEditingCurrentUser ? "Cancel" : "Edit"}
     </button>
